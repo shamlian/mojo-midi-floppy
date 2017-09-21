74abd306f6791e96c2ea8e0a4647f02303e70d26
Steven Shamlian <shamlian@users.noreply.github.com>
2017-09-21 19:03:29 -0400

    First test getting floppy drive to buzz (from 9/13)

---
+++ src/floppy.v
@@ -0,0 +1,77 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:39:24 09/14/2017 
+// Design Name: 
+// Module Name:    floppy 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module floppy(
+		input clk,
+		input enable,
+		input rst,
+		input [21:0] setpoint,
+		output step,
+		output dir,
+		output sel
+	);
+
+	reg [21:0] counter_d, counter_q;
+	reg step;
+
+	reg [6:0] dir_ctr_d, dir_ctr_q;
+	reg dir;
+
+	always @(counter_q) begin
+		counter_d = counter_q + 1'b1;
+	end
+	
+	assign sel = ~enable;
+	
+	wire int_clk = clk & enable;
+	
+	always @(posedge int_clk, posedge rst) begin
+		if (rst) begin
+			counter_q <= 22'd0;
+			step <= 1;
+		end else begin
+		   if (counter_d >= setpoint) begin
+				counter_q <= 22'd0;
+				step <= ~step;
+			end else begin
+				counter_q <= counter_d;
+			end
+		end
+	end
+
+	always @(dir_ctr_q) begin
+		dir_ctr_d = dir_ctr_q + 1'b1;
+	end
+
+	always @(negedge step, posedge rst) begin
+		if (rst) begin
+			dir_ctr_q <= 7'd0;
+			dir <= 1;
+		end else begin
+			if (dir_ctr_d == 7'd80) begin
+				dir_ctr_q <= 7'd0;
+				dir <= ~dir;
+			end else begin
+				dir_ctr_q <= dir_ctr_d;
+			end
+		end
+	end
+
+endmodule
--- src/mojo_top.v
+++ src/mojo_top.v
@@ -7,6 +7,13 @@ module mojo_top(
     input cclk,
     // Outputs to the 8 onboard LEDs
     output[7:0]led,
+    // Drive Outputs
+    output drive0_dir,
+	 output drive0_stp,
+	 output drive0_sel,
+    output drive1_dir,
+	 output drive1_stp,
+	 output drive1_sel,
     // AVR SPI connections
     output spi_miso,
     input spi_ss,
@@ -18,15 +25,44 @@ module mojo_top(
     input avr_tx, // AVR Tx => FPGA Rx
     output avr_rx, // AVR Rx => FPGA Tx
     input avr_rx_busy // AVR Rx buffer full
-    );
+	);
 
-wire rst = ~rst_n; // make reset active high
+	wire rst = ~rst_n; // make reset active high
+	
+	// hook up debug stuff
+	assign led[1] = drive0_dir;
+	assign led[0] = drive0_stp;
 
-// these signals should be high-z when not used
-assign spi_miso = 1'bz;
-assign avr_rx = 1'bz;
-assign spi_channel = 4'bzzzz;
+	// these signals should be high-z when not used
+	assign spi_miso = 1'bz;
+	assign avr_rx = 1'bz;
+	assign spi_channel = 4'bzzzz;
 
-assign led = 8'b0;
+	//assign led = 8'b0;
+	assign led[7] = rst;
+	assign led[6:2] = 5'b0;
+	
+	floppy my_floppy0 (
+		.clk(clk),
+		.enable(rst),
+		.rst(1'b0),
+		.setpoint(22'd227272),
+		.step(drive0_stp),
+		.dir(drive0_dir),
+		.sel(drive0_sel)
+	);
+	floppy my_floppy1 (
+		.clk(clk),
+		.enable(rst),
+		.rst(1'b0),
+		.setpoint(22'd191112),
+		.step(drive1_stp),
+		.dir(drive1_dir),
+		.sel(drive1_sel)
+	);
+	
+	// const = 50 MHz / [desired freq] / 2
+	// middle c 261.6 Hz 22'd95556
+	// a440 22'd56818
 
 endmodule
